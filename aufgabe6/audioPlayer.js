4068b231d9c92e2dae7cf086421399ad6143945b
Alex <spamthatshit@outlook.com>
2018-10-12 20:12:33 +0200

    first attempt on audioPlayer js

--- aufgabe6/audioPlayer.js
+++ aufgabe6/audioPlayer.js
@@ -1,1 +1,23 @@
-var context = new AudioContext;
+var context = new AudioContext,
+    gain = context.createGain(),
+    pan = context.createStereoPanner(),
+    delay = context.createDelay(),
+    isPlaying = false
+    audioBuffer;
+    request = new XMLHttpRequest();
+
+request.open('GET', "song.wav");
+request.responseType = "arraybuffer";
+request.onload = function() {
+    var undecodedAudio = request.response;
+    context.decodeAudioData(undecodedAudio, function(buffer){
+        audioBuffers = buffer;
+    });
+};
+request.send();
+
+
+
+delay.connect(gain);
+gain.connect(pan);
+pan.connect(context.destination);
